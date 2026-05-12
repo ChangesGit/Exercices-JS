425d4fb7e7cfada04dec54c5f3086ab2dbfee70f
ChangesGit <akrour.adel.pro@gmail.com>
2026-05-12 15:26:58 +0200

    FEAT : Exercice foncctions

---
+++ Exercice_arrays/main.js
@@ -0,0 +1,12 @@
+let name = "Titou";
+let age = 23;
+let passions = ["foot", "paris sportifs"];
+let tabUser = [
+    name,
+    age,
+    passions,
+];
+
+console.log(tabUser);
+console.log(tabUser[2]);
+console.log(tabUser[2][1]);
---
+++ Exercice_fonctions/main.js
@@ -0,0 +1,8 @@
+function addition33(nombre = 0) {
+    console.log(nombre + 33)
+}
+
+const addition = (a = 0,b = 0) => console.log(a+b);
+
+addition(3,4);
+addition33(32)
